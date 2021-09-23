db157b33f751fc9e8d8b2bb45b18dd944aee7eee
Laszlo Kiss <boltfigyelo@gmail.com>
2021-09-23 11:15:24 +0200

    add word upper and lowercase function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { DefaultDeserializer } = require('v8');
 
 function getWords()
 {
@@ -9,7 +10,13 @@ const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), '
 return json;
 
 }
-
+/**
+ * @function createRandomNummer
+ * @description Creates a random Number with min und max value
+ * @param {*} maxNumber 
+ * @param {*} minNumber 
+ * @returns 
+ */
 function createRandomNumber (maxNumber, minNumber)
 {
     const num = Math.floor(Math.random()* (maxNumber - minNumber) + minNumber);
@@ -17,3 +24,24 @@ function createRandomNumber (maxNumber, minNumber)
     return num;
 }
 createRandomNumber(10000, 50000);
+
+ /**
+  * @function ertellt from word erst upper und rest lowercase
+  * @param {*} word 
+  * @returns 
+  */
+
+function capitalizeString(word)
+{
+    if ( word === undefined || word.length === 0 || word == typeof(Number))
+    {
+        return "Default";
+    }
+
+    const wordStart = word.charAt(0).toUpperCase();
+    const wordRest = word.substring(1).toLowerCase();
+
+    return wordStart+wordRest;
+}
+
+console.log(capitalizeString("kismikulas"));
