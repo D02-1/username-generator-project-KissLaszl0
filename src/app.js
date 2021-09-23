d58e7c8e04e68e70ea4efe9e9ce2101d6438894b
Laszlo Kiss <boltfigyelo@gmail.com>
2021-09-23 10:55:13 +0200

    zufallNumber Function

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const path = require('path');
 
 function getWords()
 {
+    
 const json = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'words.json'), 'utf-8'));
+// console.log(path.resolve(__dirname, 'words.json'));
+return json;
 
-console.log(path.resolve(__dirname, 'words.json'));
-
-
-//console.log(json);
 }
 
-getWords();
+function createRandomNumber (maxNumber, minNumber)
+{
+    const num = Math.floor(Math.random()* (maxNumber - minNumber) + minNumber);
+  
+    return num;
+}
+createRandomNumber(10000, 50000);
